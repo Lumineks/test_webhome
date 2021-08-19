c714bfb6ac427b222d1895584abdb53567e143b7
Michail <mihasik323@gmail.com>
2021-08-19 18:11:12 +0300

    Added form

--- src/App.js
+++ src/App.js
@@ -1,5 +1,47 @@
+import { useEffect } from 'react';
+import './App.css';
+
 function App() {
-  return <div className='App'>Test task app</div>;
+  
+
+  return (
+    <div className='App'>
+      <div className='container'>
+        <h1 className='title'>Тестовое задание</h1>
+        <form className='form'>
+          <div className='form-row'>
+            <label className='form-label' htmlFor='field-username'>
+              Введите ваше имя
+            </label>
+            <input
+              className='form-input'
+              id='field-username'
+              name='name'
+              type='text'
+              required
+              placeholder='Иван'
+            />
+          </div>
+          <div className='form-row'>
+            <label className='form-label' htmlFor='field-text'>
+              Введите ваше сообщение
+            </label>
+            <textarea
+              className='form-textarea'
+              id='field-text'
+              name='text'
+              required
+              placeholder='Ваш комментарий'></textarea>
+          </div>
+          <button className='form-btn button' type='submit'>
+            Отправить
+          </button>
+        </form>
+
+        <div className='list'></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
