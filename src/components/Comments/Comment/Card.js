3abb4ae1fb4bd2e3b195d0488d586d5b9300749d
Michail <mihasik323@gmail.com>
2021-08-21 14:58:09 +0300

    Added line clamp to card name + changed font weight

--- src/components/Comments/Comment/Card.js
+++ src/components/Comments/Comment/Card.js
@@ -4,7 +4,9 @@ const Card = (props) => {
   return (
     <li className='card'>
       <div className='card-top'>
-        <h3 className='card-name'>{props.comment.name}</h3>
+        <div className='card-name-wrap'>
+          <h3 className='card-name'>{props.comment.name}</h3>
+        </div>
         <span className='card-time'>{new Date(props.comment.created_at).toLocaleDateString()}</span>
       </div>
       <p className='card-text'>{props.comment.text}</p>
